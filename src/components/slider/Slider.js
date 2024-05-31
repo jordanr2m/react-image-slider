edd3c4f03bccae3f825667d6cbf301a80a945d89
Jordan J <jordanr2m@gmail.com>
2024-05-31 14:45:15 -0700

    Began working on logic and JSX for slider component.

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 import "./Slider.css";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { sliderData } from "./slider-data.js"
 
 const Slider = () => {
-  return (
-    <div>
-      <h1>Slider</h1>
-    </div>
-  )
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    return (
+        <div className='slider'>
+            <AiOutlineArrowLeft className='arrow prev' />
+            <AiOutlineArrowRight className='arrow next'/>
+
+            {/* Use map to map over data in slider array. Map must return something. We also need the index # (built in map feature) */}
+            {/* We used index for the key property. However, if you are doing something that needs to be added/deleted, do not use index for the key prop value (bc it must be unique) */}
+            {sliderData.map((slide, index) => {
+                return (
+                    <div className={index === currentSlide ? "slide current" : "slide"} key={index}>
+                        
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Slider
