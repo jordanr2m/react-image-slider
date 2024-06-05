c56f526d7b3ac8d566d1a9e8a9ff03fd1cff0fc0
Jordan J <jordanr2m@gmail.com>
2024-06-05 16:25:33 -0700

    Began adding logic for slide transitioning.

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "./Slider.css";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { sliderData } from "./slider-data.js"
 
 const Slider = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
+    const slideLength = sliderData.length;
+    // slideLength = 1 2 3
+    // currentSlide = 0 1 2
+
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide + 1)
+    } 
+
+    // const prevSlide = () => {
+    //     setCurrentSlide(currentSlide - 1)
+    // } 
+
+    useEffect(() => {
+        // set currentSlide to 0 on page load
+        setCurrentSlide(0);
+    }, []);
 
     return (
         <div className='slider'>
             <AiOutlineArrowLeft className='arrow prev' />
-            <AiOutlineArrowRight className='arrow next' />
+            <AiOutlineArrowRight className='arrow next' onClick={nextSlide} />
 
             {/* Use map to map over data in slider array. Map must return something. We also need the index # (built in map feature) */}
             {/* We used index for the key property. However, if you are doing something that needs to be added/deleted, do not use index for the key prop value (bc it must be unique) */}
@@ -21,7 +37,7 @@ const Slider = () => {
                             // React rule: everything must be housed in 1 return container (fragment in this case. could also use div)
                             <>
                                 <img src={slide.image} alt="slide" />
-                                
+
                                 <div className="content">
                                     <h2>{slide.heading}</h2>
                                     <p>{slide.desc}</p>
